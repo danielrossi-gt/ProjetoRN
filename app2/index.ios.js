7b77c8f3d47b4f331f7f545bd1becfc613f71f8b
Daniel Rossi <danielrossi@Daniels-Mac.local>
2018-08-01 18:01:22 -0300

    Apenas testando a partir da máquina de casa

--- app2/index.ios.js
+++ app2/index.ios.js
@@ -42,11 +42,9 @@ const App = () => {
 
   return (
     <View style={estiloView}>
-
       <Text style={estiloTexto}>A</Text>
       <Text style={estiloTexto2}>B</Text>
       <Text style={estiloTexto}>C</Text>
- 
     </View>
 
   );
