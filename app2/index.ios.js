09ef092abffc34afea7b357f8ce2c305792a626c
Daniel Rossi <danielrossi@Daniels-Mac.local>
2018-08-02 19:03:34 -0300

    Finalizando app Frases

--- app2/index.ios.js
+++ app2/index.ios.js
@@ -1,6 +1,6 @@
 //Import
 import React from 'react';
-import { Text, AppRegistry, View, Image, TouchableOpacity } from 'react-native';
+import { Text, AppRegistry, View, Image, TouchableOpacity, Alert } from 'react-native';
 
 //Formatações
 const Estilos = {
@@ -23,9 +23,19 @@ const Estilos = {
   }
 };
 
-const botaoPressionado = () => {
+const gerarNovaFrase = () => {
+  let numeroAleatorio = Math.random();
+  numeroAleatorio = Math.floor(numeroAleatorio * 5);
 
-}
+  const frases = Array();
+  frases[0] = 'Não existe caminho para a felicidade. Felicidadde é o caminho.';
+  frases[1] = 'Você nunca sabe a força que tem. Até ter que ser forte.';
+  frases[2] = 'Estou sempre alegre. Essa é a melhor maneira de resolver os problema da vida.';
+  frases[3] = 'O tempo dura bastante para aqueles que sabem aproveitá-lo';
+  frases[4] = 'Se você encontrar um caminho sem obstáculos, ele provavelmente não leva a lugar nenhum.';
+
+  Alert.alert(frases[numeroAleatorio]);
+};
 
 //Criar o componente
 const App = () => {
@@ -36,7 +46,9 @@ const App = () => {
 
       <Image source={require('./imgs/logo.png')} />
 
-      <TouchableOpacity style={botao}>
+      <TouchableOpacity 
+        onPress={gerarNovaFrase}
+        style={botao}>
         <Text style={textoBotao}>Nova Frase</Text>
       </TouchableOpacity>
 
