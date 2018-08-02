48671c6068a0d4a4e5b3d90d45e79b2fddf50442
Daniel Rossi <danielrossi@Daniels-Mac.local>
2018-08-02 18:14:57 -0300

    Montando a Interfaco do app Frases do Dia

--- app2/index.ios.js
+++ app2/index.ios.js
@@ -1,29 +1,44 @@
 //Import
 import React from 'react';
-import { Text, AppRegistry, View, Image } from 'react-native';
+import { Text, AppRegistry, View, Image, TouchableOpacity } from 'react-native';
 
 //Formatações
 const Estilos = {
   
   principal: {
-    paddingTop: 40
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  imagem: {
-    justifyContent: 'flex-end',
-    paddingBottom: 5
+  botao: {
+    backgroundColor: '#538530',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 20
+  },
+  textoBotao: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold'
   }
 };
 
+const botaoPressionado = () => {
+
+}
+
 //Criar o componente
 const App = () => {
-  const { principal, imagem } = Estilos;
+  const { principal, botao, textoBotao } = Estilos;
 
   return (
     <View style={principal}>
 
-      <Image style={imagem} source={require('./imgs/uvas.png')} >
-        <Text>Legenda para a foto</Text>
-      </Image>
+      <Image source={require('./imgs/logo.png')} />
+
+      <TouchableOpacity style={botao}>
+        <Text style={textoBotao}>Nova Frase</Text>
+      </TouchableOpacity>
 
     </View>
 
