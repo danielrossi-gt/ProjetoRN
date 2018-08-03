f8cebd276dce4a1049db40a5e7e75155ca6ff88f
Daniel Rossi <danielrossi@gmail.com>
2018-08-03 15:20:24 -0300

    Testando o Git Desktop para Windows.

--- app3/index.ios.js
+++ app3/index.ios.js
@@ -17,7 +17,7 @@ export default class app3 extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Testando GitHub Desktop
+          Testando GitHub Desktop no Windows
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
