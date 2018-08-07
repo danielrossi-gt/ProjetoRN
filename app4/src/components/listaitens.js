b01021c72f305214e5f1b78fae6342ab61e20df2
Daniel Rossi <danielrossi@gmail.com>
2018-08-07 14:06:13 -0300

    Executar requisição HTTP e mostrar dados no console.

--- app4/src/components/listaitens.js
+++ app4/src/components/listaitens.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import {
     View
   } from 'react-native';
-
+import axios from 'axios';
 import Itens from './itens.js';
 
 class ListaItens extends Component {
+
+  componentWillMount() {
+    //Requisição HTTP
+    axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+      .then(response => { console.log(response); })
+      .catch(() => { console.log('Erro ao recuperar dados.'); });
+  }
 
   render() {
     return (
