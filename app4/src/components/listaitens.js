2c220e072c152e8b3e45df397a64f037a96348c2
Daniel Rossi <danielrossi@gmail.com>
2018-08-07 19:24:52 -0300

    Consumindo o array retornado pela request.

--- app4/src/components/listaitens.js
+++ app4/src/components/listaitens.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import {
-    View
+    View,
+    Text
   } from 'react-native';
 import axios from 'axios';
 import Itens from './itens.js';
 
 class ListaItens extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { listaItens: [] };
+  }
+
   componentWillMount() {
     //Requisição HTTP
     axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
-      .then(response => { console.log(response); })
+      .then(response => { this.setState({ listaItens: response.data }); })
       .catch(() => { console.log('Erro ao recuperar dados.'); });
   }
 
   render() {
     return (
       <View>
-		<Itens />
-		<Itens />
-		<Itens />
+        { this.state.listaItens.map(item => (<Text key={item.titulo}>{ item.titulo } </Text>)) } 
       </View>
       
     );
