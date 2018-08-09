d91e8ee78f6c05b8cbdabe338d5650342b82ade1
Daniel Rossi <danielrossi@gmail.com>
2018-08-09 19:30:14 -0300

    Definindo rotas.

--- app5/index.ios.js
+++ app5/index.ios.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
+  AppRegistry
 } from 'react-native';
 
+import { Navigator } from 'react-native-deprecated-custom-components';
+
 import CenaPrincipal from './src/components/CenaPrincipal.js';
+import CenaClientes from './src/components/CenaClientes.js';
+
 
 export default class app5 extends Component {
   render() {
     return (
-      <CenaPrincipal />
+      <Navigator 
+		initialRoute={{ id: 'a' }}
+		renderScene={(route, navigator) => {
+		/* definir a cena com base na rota */
+			if (route.id === 'a') {
+				//exibir a cena principal
+				return (<CenaPrincipal navigator={navigator} />);
+			}
+
+			if (route.id === 'b') {
+				//exibir a cena clientes
+				return (<CenaClientes />);
+			}
+		}}
+      />
     );
   }
 }
--- app5/src/components/CenaPrincipal.js
+++ app5/src/components/CenaPrincipal.js
@@ -3,7 +3,8 @@ import {
   View,
   StatusBar,
   Image,
-  StyleSheet
+  StyleSheet,
+  TouchableHighlight
 } from 'react-native';
 
 
@@ -32,7 +33,13 @@ export default class CenaPrincipal extends Component {
    
         <View style={styles.menu}>
           <View style={styles.menuGrupo}>
-            <Image style={styles.imgMenu} source={menuCliente} />
+            <TouchableHighlight
+              onPress={() => {
+                this.props.navigator.push({ id: 'b' });
+              }}
+            >
+              <Image style={styles.imgMenu} source={menuCliente} />
+            </TouchableHighlight>
             <Image style={styles.imgMenu} source={menuContato} />
           </View>
           <View style={styles.menuGrupo}>
