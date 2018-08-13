0ee3110fbb5a5c3f1ad256a093b567e371f72360
Daniel Rossi <danielrossi@gmail.com>
2018-08-13 20:00:00 -0300

    Android.

--- app6/index.android.js
+++ app6/index.android.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
+import { Router, Scene, Stack } from 'react-native-router-flux';
+
+import Principal from './src/components/Principal';
+import SobreJogo from './src/components/SobreJogo';
+import OutrosJogos from './src/components/OutrosJogos';
+
 
 export default class app6 extends Component {
   render() {
     return (
+
+    	<Router /*sceneStyle={{ paddingTop: 50}}*/>
+	   		<Stack key='root'>
+
+	    		<Scene key='principal' component={Principal} title='Cara ou Coroa' />
+	    		<Scene key='sobrejogo' component={SobreJogo} />
+	    		<Scene key='outrosjogos' component={OutrosJogos} />
+	    	</Stack>
+    	</Router>
+
+
+
     );
   }
 }
