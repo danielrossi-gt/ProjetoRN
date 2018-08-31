3459a1f3521857161a1e87eb9f507c2534d97da9
Daniel Rossi <danielrossi@gmail.com>
2018-08-31 11:50:15 -0300

    Conexão com o Firebase.

--- app8/src/App.js
+++ app8/src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 import Routes from './Routes';
 import reducers from './reducers';
 
-export default props => (
-    <Provider store={createStore(reducers)}>
-        <Routes />
-    </Provider>
-    
-);
+class App extends Component {
+
+    componentWillMount() {
+        let config = {
+            apiKey: "AIzaSyDsAKZBtGA2dXnMdrbrG1VM_jGAeQEfJjU",
+            authDomain: "whatsapp-clonedr.firebaseapp.com",
+            databaseURL: "https://whatsapp-clonedr.firebaseio.com",
+            projectId: "whatsapp-clonedr",
+            storageBucket: "whatsapp-clonedr.appspot.com",
+            messagingSenderId: "696370245057"
+          };
+          firebase.initializeApp(config);        
+    }
+
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <Routes />
+            </Provider>
+        );
+    }
+};
+
+export default App;
